89f0561fab58a4bbbc472790d9451fdc1f9e2f09
kodsurfer <kodsurfer@ya.ru>
2023-06-15 15:55:32 +0300

    add http server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
+import (
+	"net/http"
+)
+
+func main() {
+
+	serveMux := http.NewServeMux()
+
+	serveMux.HandleFunc("/", http.HandlerFunc(handler))
+
+	http.ListenAndServe(":8080", serveMux)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 
 }
