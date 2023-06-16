584b6c3e8167e1328eaf098be709ca9149b0ef7c
kodsurfer <kodsurfer@ya.ru>
2023-06-16 13:25:12 +0300

    add get method to handler

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 const defaultPort = "8080"
+
+var mq = make(map[string][]string, 0)
 
 func main() {
 	port := os.Args[1]
@@ -26,10 +30,36 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPut:
 		{
 			message := r.URL.Query().Get("v")
-			if message == "" {
+			if message != "" {
+				queue := r.URL.Path
+				if _, ok := mq[queue]; !ok {
+					mq[queue] = make([]string, 0)
+					mq[queue] = append(mq[queue], message)
+				}
+			} else {
 				w.WriteHeader(400)
 			}
 		}
+	case http.MethodGet:
+		queue := r.URL.Path
 
+		if len(mq) > 0 {
+			message := mq[queue][0]
+			mq[queue] = mq[queue][1:]
+
+			w.Header().Set("Content-Type", "application/json")
+
+			bytes, err := json.Marshal(message)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			_, err = w.Write(bytes)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			w.WriteHeader(404)
+		}
 	}
 }
